d8618495303eeec6d6dbf409024f492612f483e3
Rayyan Asrar <145535106+RayyanAsrar@users.noreply.github.com>
2026-01-15 19:51:23 +0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function greet(name:string):string{
+  return `hello, ${name}!`;
+}
+const username :string = "World";
+console.log(greet(username));
---
+++ src/unionAndAny.ts
@@ -0,0 +1,3 @@
+let subs : number | string = "22MILLION"
+let apirequests : "success"|"pending"|"error"="pending"
+let airlineSeats:"asile"|"window"|"middle"="asile"
